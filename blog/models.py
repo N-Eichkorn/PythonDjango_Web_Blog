972e39ffb521338bbe18d1d8e6e8f430afd644c9
Niklas Eichkorn <niklas.eichkorn@ametek.com>
2022-05-24 22:29:14 +0200

    Update models.py
    text fields instead of char fields

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     headline = models.CharField(max_length=60)
-    content = models.CharField(max_length=1000)
+    content = models.TextField(max_length=1000)
     date = models.DateTimeField('date published')
 
     def __str__(self):
@@ -11,7 +11,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     author = models.CharField(max_length=20)
-    content = models.CharField(max_length=200)
+    content = models.TextField(max_length=200)
     date = models.DateTimeField('date published')
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
